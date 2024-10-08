15f92d2d34cd3822d7e39224a43a4a263df0faf8
e1b22089 <e1b22089@oit.ac.jp>
2024-10-08 14:13:54 +0900

    リポジトリの初期化

---
+++ src/main/java/oit/is/moriyama/hagehage/pokkemonhg/PokkemonhgApplication.java
@@ -0,0 +1,13 @@
+package oit.is.moriyama.hagehage.pokkemonhg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokkemonhgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokkemonhgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/oit/is/moriyama/hagehage/pokkemonhg/PokkemonhgApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.moriyama.hagehage.pokkemonhg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokkemonhgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
